d4f41706f2722e63967798d8eebf092906f50262
Anurag <anuragagrawal4998@gmail.com>
2020-08-06 14:27:56 +0530

    eugfu

--- html_form/index.js
+++ html_form/index.js
@@ -2,34 +2,58 @@ const username = document.getElementById("username")
 const password = document.getElementById("password")
 const form = document.getElementById('form')
 const message = document.getElementById('errormessage')
+var crypt = {
+    secret: "CIPHERKEY",
+    encrypt: function (clear) {
+        // encrypt() : encrypt the given clear text
+
+        var cipher = CryptoJS.AES.encrypt(clear, crypt.secret);
+        cipher = cipher.toString();
+        return cipher;
+    },
+
+    decrypt: function (cipher) {
+        // decrypt() : decrypt the given cipher text
+
+        var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret);
+        decipher = decipher.toString(CryptoJS.enc.Utf8);
+        return decipher;
+    }
+};
 
 const x = readCookie('login')
+console.log(x)
 if (x) {
-    localStorage.setItem('name', x[0])
+    console.log(2)
+
+    let name = crypt.decrypt(x[0]);
+    localStorage.setItem('name', name)
     window.location.assign("intro.html")
 
 }
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (username.value == '' || username.value == null) {
-        message.innerHTML = 'Enter a Username'
+    if (username.value !== 'LTI' || username.value == null) {
+        message.innerHTML = 'Enter a valid Username'
 
 
     }
     else {
-        if (password.value == '' || password.value == null) {
+        if (password.value !== '123' || password.value == null) {
             message.innerHTML = 'Enter a password'
         }
         else {
             localStorage.setItem('name', username.value)
             window.location.assign("intro.html")
-            let a = [username.value, password.value]
+            let a = [crypt.encrypt(username.value), crypt.encrypt(password.value)]
             if (document.getElementById('checkbox').checked)
 
                 createCookie('login', a, 1)
         }
 
     }
+
+
 
 
 
